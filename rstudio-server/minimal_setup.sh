9c69c0569ef99c51b05232eefe98f2a9c39e03ba
tbdv <32847647+tbdv@users.noreply.github.com>
2018-03-14 12:00:30 -0700

    Update minimal_setup.sh

--- rstudio-server/minimal_setup.sh
+++ rstudio-server/minimal_setup.sh
@@ -7,4 +7,17 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 apt-get update
 apt-get upgrade -y
 
-apt-get -y update apt-get install -y gdal-bin libcurl4-openssl-dev libgdal-dev libgeos-dev libproj-dev libudunits2-dev make wget r-base-dev
+#install gdal with filegdb support
+wget https://github.com/Esri/file-geodatabase-api/raw/master/FileGDB_API_1.5/FileGDB_API_1_5_64gcc51.tar.gz
+wget http://download.osgeo.org/gdal/2.2.3/gdal223.zip
+unzip unzip gdal223.zip
+tar xzvf FileGDB_API_1_5_64gcc51.tar.gz
+cd FileGDB_API-64gcc51/
+export LD_LIBRARY_PATH=/home/ubuntu/src/FileGDB_API-64gcc51/lib
+sudo cp lib/*.so /usr/local/lib
+cd gdal-2.2.3/
+./configure --with-fgdb=/home/ubuntu/src/FileGDB_API-64gcc51
+make
+sudo make install
+
+apt-get -y update apt-get install -y libcurl4-openssl-dev libgeos-dev libproj-dev libudunits2-dev make wget r-base-dev
