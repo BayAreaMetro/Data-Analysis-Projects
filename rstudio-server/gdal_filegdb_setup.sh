89a5f4c2fd3aa493d228375176ec87bc40ba3e66
Tom B <buckley.tom@Gmail.com>
2018-03-23 15:03:28 -0700

    Update gdal_filegdb_setup.sh

--- rstudio-server/gdal_filegdb_setup.sh
+++ rstudio-server/gdal_filegdb_setup.sh
@@ -8,16 +8,19 @@ apt-get update
 apt-get upgrade -y
 
 #install gdal with filegdb support
+mkdir src
+cd src
 wget https://github.com/Esri/file-geodatabase-api/raw/master/FileGDB_API_1.5/FileGDB_API_1_5_64gcc51.tar.gz
 wget http://download.osgeo.org/gdal/2.2.3/gdal223.zip
 unzip unzip gdal223.zip
 tar xzvf FileGDB_API_1_5_64gcc51.tar.gz
 cd FileGDB_API-64gcc51/
-export LD_LIBRARY_PATH=/home/ubuntu/src/FileGDB_API-64gcc51/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ubuntu/src/FileGDB_API-64gcc51/lib
 sudo cp lib/*.so /usr/local/lib
 cd gdal-2.2.3/
 ./configure --with-fgdb=/home/ubuntu/src/FileGDB_API-64gcc51
 make
 sudo make install
 
-apt-get -y update apt-get install -y libcurl4-openssl-dev libgeos-dev libproj-dev libudunits2-dev make wget r-base-dev
+apt-get -y update
+apt-get install -y libcurl4-openssl-dev libgeos-dev libproj-dev libudunits2-dev
