f1d8918b873390f880fce88aca5b1b7dc0cfe7fc
Tom B <buckley.tom@Gmail.com>
2018-03-23 14:08:41 -0700

    Create odbc_setup.sh

---
+++ rstudio-server/odbc_setup.sh
@@ -0,0 +1,10 @@
+# borrowed from http://digitalitility.com/tutori-alitility/postgresql/odbc-setup-on-ubuntu-for-postgresql/
+sudo apt-get install unixodbc-bin unixodbc
+#ODBC drivers for PostgreSQL:
+sudo apt-get install odbc-postgresql
+#Apply the template file provided to setup driver entries:
+sudo odbcinst -i -d -f /usr/share/psqlodbc/odbcinst.ini.template
+#Setup the a sample DSN
+sudo odbcinst -i -s -l  -n adyoung-pg -f /usr/share/doc/odbc-postgresql/examples/odbc.ini.template
+#Now modify /etc/odbc.ini according to your DB:
+sudo nano /etc/odbc.ini
