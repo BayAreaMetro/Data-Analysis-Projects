78f2085271f7d1a52f401f976ea42323bfd0489b
tbdv <32847647+tbdv@users.noreply.github.com>
2018-03-29 16:24:23 -0700

    Update install_db_packages.R

--- rstudio-server/install_db_packages.R
+++ rstudio-server/install_db_packages.R
@@ -1,4 +1,4 @@
 common_library = "/usr/lib/R/library"
 
-install.packages(c("DBI","odbc","dbplyr","dplyr"), 
+install.packages(c("DBI","odbc","dbplyr","dplyr","tidyverse"), 
                   lib = common_library)
