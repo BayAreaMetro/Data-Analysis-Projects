81160f4caca75a1490554aa12a0580dd864f38fe
tbdv <32847647+tbdv@users.noreply.github.com>
2018-04-03 14:18:16 -0700

    Create speed_limit_summary.R

---
+++ lanes/speed_limit_summary.R
@@ -0,0 +1,37 @@
+library(odbc)
+library(DBI)
+library(dbplyr)
+library(dplyr)
+library(readr)
+
+#connect to dbs
+pg <- dbConnect(odbc::odbc(), "GISDB3")
+
+lanes_query <- read_file("speed_limit_summary.sql")
+res <- dbSendQuery(pg, lanes_query)
+dbFetch(res)
+dbClearResult(res)
+
+#group by attributes and output geometries for map views
+nw_db <- tbl(pg, "nw_tmc_speed_limit_ewkt")
+nw_df <- as.data.frame(nw_db)
+
+res <- dbSendQuery(pg, 'DROP VIEW tbuckley.nw_tmc_speed_limit_ewkt;')
+dbFetch(res)
+dbClearResult(res)
+
+library(sf)
+nw_sf <- st_as_sf(nw_df, wkt="shape_ewkt")
+
+####group by 
+nw_sf_sum <- nw_sf %>%
+  group_by(rdstmc,
+           speed,
+           speedtyp,
+           valdir,
+           verified) %>% 
+  summarise() %>%
+  as.data.frame() %>%
+  st_as_sf()
+
+st_write(nw_sf_sum, "nw_sum.gpkg", driver="GPKG")
