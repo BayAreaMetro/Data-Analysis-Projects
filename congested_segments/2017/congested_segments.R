fdf92cbf7efc8e8a806c3889121bf122b02cba84
tbdv <tbuckley@bayareametro.gov>
2018-05-29 09:55:58 -0700

    write out map and data

--- congested_segments/2017/congested_segments.R
+++ congested_segments/2017/congested_segments.R
@@ -2,11 +2,20 @@ library(DBI)
 library(RPostgreSQL)
 library(sf)
 library(dplyr)
+library(readxl)
+library(jsonlite)
 
-cs_18 <- readxl::read_excel("data/WeekdayCongestedSegmentsList_2017_v2.xlsx")
+congested_segments_file <- "data/WeekdayCongestedSegmentsList_2017_v2.xlsx" 
+database_credentials_file <- "~/credentials/tbuckley_gisdb3.json"
+
+#set the working directory to be that of the script
+#assumes user is running r studio
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+cs_18 <- readxl::read_excel(congested_segments_file)
 
 #connect to the db
-db3_cred <- jsonlite::read_json("~/credentials/tbuckley_gisdb3.json")
+db3_cred <- jsonlite::read_json(database_credentials_file)
 
 db3 <- dbConnect(RPostgreSQL::PostgreSQL(),
                  dbname = db3_cred['database'], 
@@ -27,41 +36,19 @@ dat <- dbGetQuery(db3, tmc_query)
 
 df3 <- st_as_sf(dat, wkt = "geom", crs = 3857)
 
-cs18_sf <- inner_join(df3,cs_18,by=c("tmcid"="TMC ID"))
+cs17_sf <- inner_join(df3,cs_18,by=c("tmcid"="TMC ID"))
 
-cs18_limits <- cs18_sf %>% 
-  group_by(LIMITS,startTime,endTime,`Segment Delay  (Veh-Hrs)`,Rank,`length (Miles)`) %>% 
+cs17_limits <- cs17_sf %>% 
+  group_by(LIMITS,startTime,endTime,
+           `Route #`,DIR,`Segment Delay  (Veh-Hrs)`,Rank,`length (Miles)`) %>% 
   summarise()
 
-############
-##fill holes
-############
-dim(cs18_limits)
-head(cs18_limits)
-mapview(cs18_limits)
-library(mapview)
-mapview(cs18_limits)
-st_write(cs18_limits,"cs18_limits.geojson")
-library(sf)
-st_write(cs18_limits,"cs18_limits.geojson")
-st_write(cs18_limits,"cs18_limits.gpkg")
-getwd()
-cs18_limits[121,]
-View(cs_18)
-cs18_limits[121,]$geom
+cs17_limits$start_hour <- hour(lubridate::ymd_hms(cs17_limits$startTime))
+cs17_limits$start_minute <- minute(lubridate::ymd_hms(cs17_limits$startTime))
 
-multi_121 <- cs18_limits[121,]$geom
+cs17_limits$end_hour <- hour(lubridate::ymd_hms(cs17_limits$endTime))
+cs17_limits$end_minute <- minute(lubridate::ymd_hms(cs17_limits$endTime))
 
-points_121 <- st_cast(multi_121, "MULTIPOINT")
-xy_121 <- st_coordinates(points_121)
-xy_121 <- as_tibble(xy_121)
-xy_121 <- arrange(xy_121,X,Y)
-st_linestring(xy_121)
-st_linestring(as.matrix(xy_121))
-
-
-###
-#check single
-####
-single_127 <- cs18_limits[127,]$geom
-
+m1 <- mapview(cs17_limits, zcol="Rank", legend=TRUE)
+mapshot(m1, url = paste0(getwd(), "/congested_segments_map17.html"))
+st_write(cs17_limits,"cs17_limits.geojson")
