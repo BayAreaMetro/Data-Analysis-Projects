d764ac35fd07d0b35e0aca31cf9a05ff781bf22b
tbdv <tbuckley@bayareametro.gov>
2018-06-21 11:21:07 -0700

    fixup

--- census/2018_language/get_data.R
+++ census/2018_language/get_data.R
@@ -26,8 +26,8 @@ census_vars <- c('C16001_005',
 language_table <- get_acs(geography = "tract", 
                              variables = census_vars,
                              state = "CA", county=counties,
-                             year=2015,
-                             summary_var = "B16001_001")
+                             year=2016,
+                             summary_var = "C16001_001")
 
 better_names_df <- tibble(variable = census_vars, language=shortnames)
 
@@ -36,4 +36,4 @@ language_table_named <- left_join(language_table,better_names_df) %>%
 
 language_table_wide <- language_table_named %>% spread(language, estimate)
 
-write_csv(language_table_wide,"~/Documents/Projects/BAM_github_repos/dv_temp/census/2018_language/language_table_acs_2015.csv")
+write_csv(language_table_wide,"~/Documents/Projects/BAM_github_repos/dv_temp/census/2018_language/language_table_acs_2016.csv")
